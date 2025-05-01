001dee179c919cf1968251b4b3ec5ed02c19512b
motta97 <155576275+motta97@users.noreply.github.com>
2025-05-01 22:26:01 +0300

    Add files via upload
    Initial Implementation of User, Teacher, and Student Classes

---
+++ Online Language Teaching System/src/main/java/Main/User.java
@@ -0,0 +1,25 @@
+public abstract class   User {
+    private String username;
+    private String email;
+    private String password;
+    private String ID;
+
+    public User(String username, String email, String password, String ID) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.ID = ID;
+    }
+
+    public User(){
+
+    }
+    public String getID() {
+        return ID;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
